cf824d400899c1cfbd30ff2ae009a2a278aeb5f1
Bernardo Caiado <becaiado@gmail.com>
2020-08-31 08:32:56 -0700

    added validates for coffee

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,18 @@
 class CoffeesController < ApplicationController
+  def show
+    @coffee = Coffee.find(params[:id])
+    @reviews = Review.where(coffee_id: @coffee)
+  end
+
+  def category
+    @coffee = Coffee.where("origin ILIKE ? OR roast ILIKE ? OR brand ILIKE ? OR sensory ILIKE ?", "%#{params[:format]}%")
+  end
+
+  def search
+    if params[:query].present?
+      @coffee = search_by_all(params[:query])
+    else
+      @coffee = Coffee.all
+    end
+  end
 end
--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,3 +1,13 @@
 class Coffee < ApplicationRecord
   has_many :reviews
+
+  validates %i[name origin farm brand sensory roast], presence: true
+  validates :name, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_all,
+                  against: %i[name origin farm brand sensory roast],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
