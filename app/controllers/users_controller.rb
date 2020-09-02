59c0eb399612c7bff8d79efe27704cf4e4478aa6
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-09-02 17:09:22 -0300

    fix show for visitors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:follow, :unfollow]
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
     @users = User.where.not(id: current_user.id)
