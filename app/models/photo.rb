02b4f67623c3dcfa94858ed2ac639f2ca43fdb60
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-09-07 15:50:32 -0300

    WIP

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,21 @@
 class Photo < ApplicationRecord
    has_one_attached :photo
+require "google/cloud/vision"
+
+def ocr_search(photo)
+
+image_annotator = Google::Cloud::Vision.image_annotator
+
+file_name = photo
+
+response = image_annotator.text_detection(
+  image:       file_name,
+  max_results: 3
+)
+
+response.responses.each do |res|
+  res.text_annotations.each do |text|
+    puts text.description
+  end
 end
+
