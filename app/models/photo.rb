f1d71c3c3d479ddf2ab696fe65d27334cb85e39d
Bernardo Caiado <becaiado@gmail.com>
2020-09-08 13:38:34 -0700

    acabou porra

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,10 +5,7 @@ class Photo < ApplicationRecord
   has_one_attached :photo
 
   def self.detect_text(file_name)
-    puts file_name
-    puts "----------------------------------------------------------------------------------------"
     image_annotator = Google::Cloud::Vision.image_annotator
-    puts file_name
     response = image_annotator.text_detection(
       image: file_name,
       max_results: 1 # optional, defaults to 10
@@ -19,7 +16,6 @@ class Photo < ApplicationRecord
       res.text_annotations.each do |text|
         test << text.description
       end
-      puts response
       return test[0].split("\n").first
     end
   end
