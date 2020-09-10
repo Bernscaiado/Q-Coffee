9dc9648ceec570a800e410b8a2aed533a1ae2f3a
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-09-09 22:01:49 -0300

    anybody can see maps

--- app/controllers/review_likes_controller.rb
+++ app/controllers/review_likes_controller.rb
@@ -17,22 +17,4 @@ class ReviewLikesController < ApplicationController
       format.js
     end
   end
-
-  # def create
-  #   @review_like = ReviewLike.new
-  #   @review_like.user = current_user
-  #   @review_like.review = Review.find(params[:review_id])
-  #   @review_like.save!
-  #   flash[notice] = 'Review curtida'
-  #   redirect_to coffee_path(@review_like.coffee)
-  # end
-
-  # def destroy
-  #   @review = Review.find(params[:id])
-  #   @review_like = ReviewLike.find_by(params[:review_like_id], current_user)
-  #   @coffee = @review.coffee
-  #   @review_like.destroy
-  #   flash[:notice] = 'Café desfavoritado'
-  #   redirect_to coffee_path(@coffee)
-  # end
 end
