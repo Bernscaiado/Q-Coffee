c5e48f48ba0148cf2551ace2c7febb9e0acb3a23
Bernardo Caiado <becaiado@gmail.com>
2020-09-10 12:10:45 -0700

    fixed minor bugs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :coffee
-  has_many :review_likes
+  has_many :review_likes, dependent: :destroy
 
   validates :content, presence: true
   validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
